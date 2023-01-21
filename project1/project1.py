7150ab62a5eb2602af816287e995ef669bf1ef85
Angel <angelrodriguez0617@gmail.com>
2023-01-20 22:42:07 -0700

    Plot created

--- project1/project1.py
+++ project1/project1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import statistics 
+from matplotlib import pyplot as plt 
 
 df = pd.read_excel(r'project1/BTC-USD.xlsx')
 print(df)
@@ -8,5 +9,12 @@ dates = np.array(df['Date'])
 closes = np.array(df['Close'])
 mean = statistics.mean(closes)
 stdev = statistics.stdev(closes)
-print(f"The mean of the closing prices is {mean}")
-print(f"The mean of the closing prices is {stdev}")
+variance = statistics.variance(closes)
+print(f"The mean of the closing prices is: {mean}")
+print(f"The mean of the closing prices is: {stdev}")
+print(f"The mean of the closing prices is: {variance}")
+plt.title("Matplotlib demo") 
+plt.xlabel("Time (days)") 
+plt.ylabel("Close Price (USD)") 
+plt.plot(closes) 
+plt.show()
