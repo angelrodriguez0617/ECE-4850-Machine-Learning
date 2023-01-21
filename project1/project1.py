1c4a299f53c475e611c25576a20d346edd4891a5
Angel <angelrodriguez0617@gmail.com>
2023-01-20 22:24:02 -0700

    Read from xlsx and got standard deviation as well as mean

--- project1/project1.py
+++ project1/project1.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import numpy as np
+import statistics 
 
 df = pd.read_excel(r'project1/BTC-USD.xlsx')
 print(df)
+dates = np.array(df['Date'])
+closes = np.array(df['Close'])
+mean = statistics.mean(closes)
+stdev = statistics.stdev(closes)
+print(f"The mean of the closing prices is {mean}")
+print(f"The mean of the closing prices is {stdev}")
