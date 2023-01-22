f2b51d90455b3dfc48a75ecb22d4faf06d05e133
Angel <angelrodriguez0617@gmail.com>
2023-01-22 15:20:25 -0700

    syncing newest changes

--- project1/project1.py
+++ project1/project1.py
@@ -1,19 +1,17 @@
 import pandas as pd
 import numpy as np
-import statistics 
 from matplotlib import pyplot as plt 
 
 df = pd.read_excel(r'project1/BTC-USD.xlsx')
 print(df)
 dates = np.array(df['Date'])
 closes = np.array(df['Close'])
-mean = statistics.mean(closes)
-stdev = statistics.stdev(closes)
-variance = statistics.variance(closes)
+mean = sum(closes) / closes.size
+alpha = closes - mean
+variance = np.dot(alpha.T, alpha) / closes.size
 print(f"The mean of the closing prices is: {mean}")
-print(f"The mean of the closing prices is: {stdev}")
 print(f"The mean of the closing prices is: {variance}")
-plt.title("Matplotlib demo") 
+plt.title("Bitcoin Data") 
 plt.xlabel("Time (days)") 
 plt.ylabel("Close Price (USD)") 
 plt.plot(closes) 
