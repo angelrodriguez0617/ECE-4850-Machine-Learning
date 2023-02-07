99e6c7e3346891c09218a8134594efa1e4f756c6
Angel <angelrodriguez0617@gmail.com>
2023-02-07 16:24:45 -0700

    added coordinates on the plots

--- project2/project2.py
+++ project2/project2.py
@@ -92,9 +92,11 @@ while T > T_stop:
                    ypath[1:]-ypath[:-1],scale_units='xy', angles='xy', scale=1, color='teal', width=0.005)
     # re-drawing the figure
     plt.title("Traveling Salesman Path")
-    plt.text(15, 90, f'Energy: {int(energy)}', fontsize='medium', weight="bold")
+    for i, j in zip(xpath, ypath): # Writes the (x,y) coordinates above the coordinate location
+            plt.text(i, j, '({}, {})'.format(i, j), fontsize='small')
+    plt.text(10, 90, f'Energy: {int(energy)}', fontsize='medium', weight="bold")
     format_T = "{:.3f}".format(T)
-    plt.text(15, 85, f'Temperature: {format_T}', fontsize='medium', weight="bold")
+    plt.text(10, 85, f'Temperature: {format_T}', fontsize='medium', weight="bold")
     fig.canvas.draw()
     # to flush the GUI events
     fig.canvas.flush_events()
@@ -102,8 +104,10 @@ while T > T_stop:
     time.sleep(0.1)
 
 # print(f"best_iterator: {best_iterator}")
-# lowest_energy = total_energy(xpos,ypos,best_iterator)
-# print(f"lowest_energy: {lowest_energy}")
+lowest_energy = total_energy(xpos,ypos,best_iterator)
+print(f"lowest_energy: {lowest_energy}")
+print(f"min value in energy_list: {np.amin(energy_list)}")
+energy = total_energy(xpos,ypos,best_iterator)
 
 # Turn off the fast updating plot
 plt.ioff()
@@ -123,8 +127,9 @@ plt.plot(xpath,ypath)
 plt.quiver(xpath[:-1], ypath[:-1], xpath[1:]-xpath[:-1], 
                    ypath[1:]-ypath[:-1],scale_units='xy', angles='xy', scale=1, color='teal', width=0.005)
 plt.title("Shortest Traveling Salesman Path Found")
-plt.text(15, 90, f'Total Energy: {int(np.amin(energy_list))}', fontsize='medium', weight="bold")
-format_T = "{:.3f}".format(T)
+for i, j in zip(xpath, ypath): # Writes the (x,y) coordinates above the coordinate location
+            plt.text(i, j, '({}, {})'.format(i, j), fontsize='small')
+plt.text(15, 90, f'Energy: {int(energy)}', fontsize='medium', weight="bold")
 plt.subplots_adjust(left=0.125, bottom=0.044, right=0.589, top=0.943, wspace=0.2, hspace=0.291)
 manager = plt.get_current_fig_manager()
 manager.full_screen_toggle() # Make full screen for better view, Alt-F4 to exit full screen
