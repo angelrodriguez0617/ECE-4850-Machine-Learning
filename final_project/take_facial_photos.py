30f2ad4c0f1e0c3bacc2597b07b5f044a811a147
angelrodriguez0617 <angelrodriguez0617@gmail.com>
2023-03-16 20:24:28 -0600

    cleaned up code

--- final_project/take_facial_photos.py
+++ final_project/take_facial_photos.py
@@ -20,63 +20,61 @@ face = 1
 angle = 0
 
 path = "Default"
-parent_directory = os.getcwd()
+# Use current working directory
+# parent_directory = os.getcwd()
+# Use directory of the script
+parent_directory = os.path.realpath(os.path.dirname(__file__))
+# Name of directory to save the video and pictures into 
 directory = str(faces[face] + "_" + angles[angle])
 path = os.path.join(parent_directory, directory)
 if os.path.isdir(directory):
     shutil.rmtree(directory)
+# Create directory to save video and pictures into
 os.mkdir(path)
 os.chdir(path)
 
-def recordVideo(video, face, angle, img_num=1):
-    '''File each step of the path
-    img_num represents the number of the photo that is being taken since program start'''    
-    #record = 150
-    # IDK why, but *'XVID' is no longer working, instead I am using *'mp4v'
-    # video = cv.VideoWriter(f'{face}_{angle}_{img_num}.mp4', cv.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+def extract_pictures(video, face, angle):
+    '''Extract and save photos from passed video parameter'''    
+    # img_num is to give each picture a unique name so pictures are not overriden
+    img_num=1
     count = 0
-    while True: #record > 0:
+    while True:
         if count%30 == 0:
+            # Save picture  
             cv.imwrite(f'{face}_{angle}_img{img_num}.png', frame_read.frame)
             img_num += 1
         count += 1
         video.write(frame_read.frame)
         sleep(1/60)
-        #record -= 1
-    #return img_num
 
 if __name__ == "__main__":
-    # Initialize drone object and turn on
+    # Turn on drone
     drone = Tello()
     drone.connect()
     drone.streamon()
     # Austin says this might work
     sleep(0.1)
 
-    camera = drone
-    if camera.get_battery() < 20: # if battery is under 20%
+    if drone.get_battery() < 20: # if battery is under 20%
         print("\n" + ">>>>>>>>>>>>>>>> DRONE BATTERY LOW. CHANGE BATTERY!")
 
-    # Record video and pictures
-    
     try:
-        frame_read = camera.get_frame_read()
+        frame_read = drone.get_frame_read()
         height, width, _ = frame_read.frame.shape
+        # Record video and choose name to save it under
         video = cv.VideoWriter(f'{faces[face]}_{angles[angle]}_video.mp4', cv.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
-        video_thread = threading.Thread(target=recordVideo, args=(video, faces[face], angles[angle],))
+        # Create thread to extract pictures from video while doing other processes
+        video_thread = threading.Thread(target=extract_pictures, args=(video, faces[face], angles[angle]))
         video_thread.start()
-        # img_num should equal 10 after this meaning 10 photos were taken 
 
-        while True:
-            frame_read = camera.get_frame_read()
+        while True: # Show the user live feed from drone until pressing ctrl + c
+            frame_read = drone.get_frame_read()
             img = frame_read.frame
             cv.imshow("Output", img)
             cv.waitKey(1)
 
-        # video.release()
-        # sys.exit()
-
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: # Supposed to come here after pressing ctrl + c
+        # Video must be released, otherwise, saved video will be corrupted.
         video.release()
         sys.exit()
 
