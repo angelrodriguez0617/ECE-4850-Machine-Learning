3f9035b3d83708382dd31bcee692d9d125b5864d
Angel <angelrodriguez0617@gmail.com>
2023-03-21 21:53:58 -0600

    replaced take_facial_photos with Austin's slightly edited code

--- final_project/take_facial_photos.py
+++ final_project/take_facial_photos.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 from time import sleep
-import cv2 as cv
 import shutil
 import threading
 import os
@@ -9,12 +8,12 @@ import sys
 
 
 # This will be used to specify which person the recording will be of
-faces = ["Angel", "Austin", "Shekaramiz"] 
-# This will be used tos specify which facial angle will be recorded
-angles = ['smiling', 'non-smiling']
+faces = ["Angel_smiling", "Austin_smiling", "Shekaramiz_neutral", "wall"] 
+# This will be used to specify which facial angle will be recorded
+angles = ["front", "left", "right"]
 # These are variable to adjust depending on whose face we are recording and which angle it is
 face = 0
-angle = 1 
+angle = "profile"
 
 path = "Default"
 # Use current working directory
@@ -22,7 +21,7 @@ path = "Default"
 # Use directory of the script
 parent_directory = os.path.realpath(os.path.dirname(__file__))
 # Name of directory to save the video and pictures into 
-directory = str(faces[face] + "_" + angles[angle])
+directory = str(faces[face] + "_" + angle)
 path = os.path.join(parent_directory, directory)
 if os.path.isdir(directory):
     shutil.rmtree(directory)
@@ -36,13 +35,18 @@ def extract_pictures(video, face, angle):
     img_num=1
     count = 0
     while True:
-        if count%30 == 0:
+        if count%10 == 0:
             # Save picture  
             cv.imwrite(f'{face}_{angle}_img{img_num}.png', frame_read.frame)
             img_num += 1
         count += 1
         video.write(frame_read.frame)
         sleep(1/60)
+        if img_num >= 200:
+            print("Enough Photos")
+            os.system("speaker-test -t sine -f 1000 -l 1 & sleep .2 && kill -9 $!")
+            video.release()
+            quit()
 
 if __name__ == "__main__":
     # Turn on drone
@@ -52,17 +56,16 @@ if __name__ == "__main__":
     # Austin says this might work
     sleep(0.1)
 
-    print(f'>>>>>>>>>>>>>>>> DRONE BATTERY: {drone.get_battery()}')
     if drone.get_battery() < 20: # if battery is under 20%
-        print(">>>>>>>>>>>>>>>> DRONE BATTERY LOW. CHANGE BATTERY!")
+        print("\n" + ">>>>>>>>>>>>>>>> DRONE BATTERY LOW. CHANGE BATTERY!")
 
     try:
         frame_read = drone.get_frame_read()
         height, width, _ = frame_read.frame.shape
         # Record video and choose name to save it under
-        video = cv.VideoWriter(f'{faces[face]}_{angles[angle]}_video.mp4', cv.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
+        video = cv.VideoWriter(f'{faces[face]}_{angle}_video.mp4', cv.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
         # Create thread to extract pictures from video while doing other processes
-        video_thread = threading.Thread(target=extract_pictures, args=(video, faces[face], angles[angle]))
+        video_thread = threading.Thread(target=extract_pictures, args=(video, faces[face], angle))
         video_thread.start()
 
         while True: # Continue showing live feed from drone until pressing ctrl + c
